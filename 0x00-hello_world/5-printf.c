b30f3517c70a51a21b154d85fab1881b1f8dda40
Ufoma Genesis <ufoma.genesis@gmail.com>
2023-08-10 21:29:11 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program that uses the function printf'
+ * return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
