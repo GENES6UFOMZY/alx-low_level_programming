50e03c5ab2c0ff53b5b369ded671a77583d12b09
Ufoma Genesis <ufoma.genesis@gmail.com>
2023-08-14 10:32:38 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - hexadecimal
+ * Return: 0-(success)
+ */
+int main(void)
+{
+	int num = 0;
+	char alph = 'a';
+
+		while (num < 10)
+		{
+			putchar('0' + num);
+			num++;
+		}
+		while (num >= 10 && num < 16)
+		{
+			putchar(alph);
+			alph++;
+			num++;
+		}
+	
+	putchar('\n');
+	return (0);
+}
