569a75479f4575eefcfb566f3f722c972f448d80
Ufoma Genesis <ufoma.genesis@gmail.com>
2023-08-14 08:09:51 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - printing first 10 numbers
+ * Return: 0-(Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+	putchar('\n');
+	return (0);
+}
