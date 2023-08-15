977b703359964ebcb4a6167fa4a58bf521af7bbb
Ufoma Genesis <ufoma.genesis@gmail.com>
2023-08-14 18:52:53 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,9 +7,10 @@ int main(void)
 {
 	int i = 0;
 	int j = 0;
+
 	for (i = 0; i < 9; i++)
 	{
-		for (j = i+1; j <= 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
 			putchar ('0' + i);
 			putchar('0' + j);
