0a9c3e652d043dd682e4a42381368ad620e4c644
Ufoma Genesis <ufoma.genesis@gmail.com>
2023-08-14 18:50:48 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - printing all 2 digits numbers
+ * Return: 0-(Success)
+ */
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i+1; j <= 9; j++)
+		{
+			putchar ('0' + i);
+			putchar('0' + j);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
