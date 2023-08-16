9856a30569787badfcb2454795c11fd87f8915cb
Ufoma Genesis <ufoma.genesis@gmail.com>
2023-08-16 04:01:52 -0700

    2-alphabetx10

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: 0-Always Success
+*/
+int main(void){
+/*	_putchar();*/
+	print_alphabet_x10();
+	return (0);
+}	
