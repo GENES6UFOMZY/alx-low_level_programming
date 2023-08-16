3a94e07004a74705d4e5e0b0b98228ee7bff5fb5
Ufoma Genesis <ufoma.genesis@gmail.com>
2023-08-16 07:57:45 -0700

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - Entry poing
+ * @n: variable int
+ * Return: 1 positive 0, zero,1- negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	else if (n == 0)
+	{
+	_putchar(48);
+	return (0);
+	}
+	else
+	{
+	_putchar(45);
+	return (-1);
+	}
+}
+
