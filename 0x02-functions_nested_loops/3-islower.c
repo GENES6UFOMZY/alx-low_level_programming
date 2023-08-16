9ad6bae1bc1282dbece3d78db2bef52f6b1c465b
Ufoma Genesis <ufoma.genesis@gmail.com>
2023-08-16 05:39:26 -0700

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * _islower - checks for lower case character
- * eturn: 1 -for  lowercase
- * Return: 0 - for anyhting else
+ * @c -ascii value for character
+ * Return: 1-for lowercase and 1 - for anyhting else
  */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-		return (1);	
+	return (1);
 	}
 	return (0);
 }
