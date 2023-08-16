79697da7a6c8f10f215646a5828956e3bfe1b3c7
Ufoma Genesis <ufoma.genesis@gmail.com>
2023-08-16 12:34:00 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs -to get the absolte value
+ * @a: variable
+ * Return: a for any value
+ */
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		a = a * -1;
+	return (a);
+	}
+	else
+	{
+	return (a);
+	}
+}
