31854ec0cf7ba9a50d60857cf515af8432bb6555
Ufoma Genesis <ufoma.genesis@gmail.com>
2023-08-16 21:42:49 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add -adding two numbers
+ * Return: sum
+ *@a: first nmber
+ *@b: second number
+ */
+
+int add(int a, int b)
+{
+	int sum;
+	sum = a + b;
+	return (sum);
+}
